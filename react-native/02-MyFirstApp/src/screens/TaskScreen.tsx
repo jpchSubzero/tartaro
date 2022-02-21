2d8bedef22c6b1374ed53a5c71f485a1385ec1ab
Juan Pablo Correa H <56457468+jpchSubzero@users.noreply.github.com>
2022-02-20 22:09:20 -0500

    quitar dimensions

--- react-native/02-MyFirstApp/src/screens/TaskScreen.tsx
+++ react-native/02-MyFirstApp/src/screens/TaskScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dimensions, View } from 'react-native';
+import { View } from 'react-native';
 import { styles } from './TaskStyles/10';
 
 export const TaskScreen = () => {
